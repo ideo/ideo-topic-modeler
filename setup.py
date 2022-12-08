b1c3a0e0605c4748ea1acaedef7c195633e8aeb1
Joe Gambino <joe@gam.bingo>
2022-12-08 16:34:32 -0600

    testing without setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-
-setup(
-    name='IDEO-Topic-Modler',
-    version='0.1dev',
-    packages=['ideo_topic_modeler',],
-    license='Creative Commons Attribution-Noncommercial-Share Alike license',
-    long_description=open('README.md').read(),
-)
