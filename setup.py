82224b45b1cfd09ccd773c1d717375982ded44af
Joe Gambino <joe@gam.bingo>
2022-12-08 16:47:34 -0600

    install requires

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 from distutils.core import setup
 
 setup(
-    name='IDEO-Topic-Modeler',
+    name='ideo_topic_modeler',
     version='0.1dev',
     packages=['ideo_topic_modeler',],
+    install_requires=[
+        "pandas",
+        "altair",
+        "matplotlib",
+        "sentence-transformers",
+        "bertopic",
+        "umap",
+      ],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
 )
